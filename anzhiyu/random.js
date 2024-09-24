2ae24b0bb4321040994d7a01c4599ece8126e219
Kn1ght814 <2115816354@qq.com>
2024-09-24 21:57:04 +0800

    Site updated: 2024-09-24 21:57:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/24/hello-world/","2024/08/26/我的第一篇博客/"];function toRandomPost(){
+var posts=["2024/08/24/hello-world/","2024/08/26/我的第一篇博客/","2024/09/24/小迪第二天/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
