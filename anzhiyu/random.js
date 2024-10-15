b34316f461503723888770292dc31e29f2326657
Kn1ght814 <2115816354@qq.com>
2024-10-16 01:24:56 +0800

    Site updated: 2024-10-16 01:24:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/SHCTF-week1-复现/","2024/08/11/hello-world/","2024/08/24/我的第一篇博客/","2024/08/24/小迪第二天/"];function toRandomPost(){
+var posts=["2024/10/12/SHCTF-week1-复现/","2024/08/11/hello-world/","2024/08/24/我的第一篇博客/","2024/08/24/小迪第二天/","2024/10/16/区块链入门-0xGame-Blockchain/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
